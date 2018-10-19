3e03a857f515adbcf4ff2389ed13509b70acc025
s33dco <code@s33d.co>
2018-10-19 10:18:05 +0100

    test POST todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const moment = require('moment');
+const mongoose  = require('mongoose');
+const moment    = require('moment');
 
 
 mongoose.Promise = global.Promise;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express   = require('express');
-const bodyParser= require('body-parser');
+const express     = require('express');
+const bodyParser  = require('body-parser');
 
 
-let {mongoose}  = require('./db/mongoose');
-let {Todo}      = require("./models/todo");
-let {User}      = require("./models/user");
+const {mongoose}  = require('./db/mongoose');
+const {Todo}      = require("./models/todo");
+const {User}      = require("./models/user");
 
 let app = express();
 
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
