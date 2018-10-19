b02d020e0125244cadddb8106282ed07ba775761
s33dco <code@s33d.co>
2018-10-19 12:50:37 +0100

    add GET/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
-
+const {ObjectID}  = require('mongodb');
 
 const {mongoose}  = require('./db/mongoose');
 const {Todo}      = require("./models/todo");
@@ -21,12 +21,29 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e.message);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
