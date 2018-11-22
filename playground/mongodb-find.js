3636742d612c332ecba340e62d477dd4b4453e2c
s33dco <code@s33d.co>
2018-11-22 08:41:36 +0000

    revise mongo driver

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,8 +7,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp')
 
-  db.collection('Todos').find({_id: new ObjectID('5bc7242ad7bd7dba3b4a5fa9')})
-    .toArray().then((docs) => {
+  db.collection('Todos').find({
+    _id: new ObjectID('5bc7242ad7bd7dba3b4a5fa9')
+  }).toArray().then((docs) => {
       console.log('Todos');
       console.log(JSON.stringify(docs, undefined, 2));
     }, (err) => {
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose  = require('mongoose');
-const moment    = require('moment');
-
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',  { useNewUrlParser: true });
