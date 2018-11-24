b78e885dfb8ceb87e010ab8ec9f9478dbc969dda
s33dco <code@s33d.co>
2018-11-24 00:22:23 +0000

    set up test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose  = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',  { useNewUrlParser: true }); // use heroku mlab addon || localhost db
-
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true }); 
 module.exports = {mongoose};
